2782dca15c98cd5ad54e03216c061577f5c66758
acedeywin <40429848+acedeywin@users.noreply.github.com>
2018-07-01 15:58:05 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,38 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+var height, width, color;
+
+$( "#sizePicker" ).submit(function( event ) {
+    event.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWeight").val();
+    makeGrid(height, width);
+
+
+  });
+
+
+function makeGrid(height, width) {
+$('table tr').remove();
+
+for (var i= 1; i <= height; i++ ){
+  $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+  for (var j= 1; j <= width; j++ ){
+    $('#table' + i).append('<td></td>');
+  }
+}
+
+$('td').click(function addColor() {
+  color = $('#colorPicker').val();
+
+  if($(this).attr('style')){
+    $(this).removeAttr('style')
+  }else{
+    $(this).attr('style','background-color:' + color)
+  }
+
+})
+}
